0bd27baa7ac0a52ab0b6333a29ebbf5cef7dc2fa
Elvin <150334709+elayy-e@users.noreply.github.com>
2024-05-23 16:49:20 +0400

    ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,35 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## Call "MASS" library for running ginv function
 makeCacheMatrix <- function(x = matrix()) {
-
-}
-
+nv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinv <- function(inverse) inv <<- inverse
+  getinv <- function() {
+    invert <- ginv(x)  ## can get information by typing "?ginv" in console
+    invert %*% x
+  }
+  list(set = set, get = get,
+       setinv = setinv,
+       getinv = getinv)
+}	
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+   inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data!")
+    return(inv)
+  }
+  invert_data <- x$get()
+  inv <- solve(invert_data, ...)
+  x$setinv(inv)
+  inv
 }
